d955c4c21c114f9198050128d0277d07a6d91fb7
accartwr <89925308+accartwr@users.noreply.github.com>
2023-03-05 17:23:59 -0500

    Navigation bar code only
    Save an image as profileImage.png in the same directory to display as link to profile page

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<nav>
+	<ul>
+		<li><a href="homepage.php">Home</a></li>
+		<li><a href="schedule.php">Schedule</a></li>
+		<li><a href="calendar.php">Calendar</a></li>
+		<li><a href="inventory.php">Inventory</a></li>
+		<li><a href="expenses.php">Expenses</a></li>
+    <li><a href="profile.php"><img src="profileImage.png" alt="Profile"></a></li>
+	</ul>
+</nav>
+
+<!-- Can save an image as profileImage.png in this same directory to display as the button for profile page -->
