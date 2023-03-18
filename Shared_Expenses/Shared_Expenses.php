05b74ddea7bb2fb71e1f3e1b52c4730d791d929a
Arpit Hirani <70348757+arpithirani@users.noreply.github.com>
2023-03-17 21:44:37 -0400

    Worked on displaying all the Bills and Expense Details #42
    The commit includes updated code for Shared Expenses as well as A new file name Display Expenses that shows the share for each roommate. It is opened when the user clicks on any bill.

--- Shared_Expenses/Shared_Expenses.php
+++ Shared_Expenses/Shared_Expenses.php
@@ -1,6 +1,5 @@
 <?php
 
-//include "process-form.php";
 // session_start();
 //$current_user = $_SESSION['username'];
 
@@ -17,9 +16,9 @@ if (!$conn) {
 
 <?php
 // $sql = "SELECT * FROM roomAidExpenses WHERE username = '$current_user'";
-//using 'hGilmore999' for testing purposes'
-    $sql = "SELECT * FROM allExpenses WHERE username = 'hGilmore909' ";
-    $graph = mysqli_query($conn, $sql);
+//using 'hGilmore909' for testing purposes and we will replace that with current user after make group formation.'
+$sql = "SELECT * FROM allExpenses WHERE (username = 'hGilmore909' OR user2 = 'hGilmore909' OR user3 = 'hGilmore909' OR user4 = 'hGilmore909')";
+$graph = mysqli_query($conn, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +28,7 @@ if (!$conn) {
     <title>Shared Expenses</title>
 
     <link rel="stylesheet" href="Shared_Expenses.css">
-
+<!--Using Google Developers Chart API to represent the chart for user expenses.-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
@@ -38,11 +37,12 @@ if (!$conn) {
             var data = google.visualization.arrayToDataTable([
                 ['Expense Name', 'Total Amount'],
                 <?php
-            while ($chart = mysqli_fetch_assoc($graph)) {
-                echo "['".$chart['expenseName']."',".$chart['totalAmt']."],";
-            }
+                while ($chart = mysqli_fetch_assoc($graph)) {
+                    echo "['".$chart['expenseName']."',".$chart['totalAmt']."],";
+                }
                 ?>
-        ]);
+            ]);
+            //Formatting the Chart
             var options = {
                 backgroundColor: '#CDB7E9',
                 width: 645,
@@ -55,10 +55,11 @@ if (!$conn) {
         }
     </script>
 
-<!--The script for Add Bill Form-->
+    <!--The script for Add Bill Form-->
     <script>
         function openBillForm() {
             document.getElementById("form-container").style.display = "block";
+            document.getElementById("info-from").style.display = "none";
         }
         function closeBillForm() {
             document.getElementById("form-container").style.display = "none";
@@ -67,11 +68,23 @@ if (!$conn) {
         function textClear() {
             var frm=document.getElementsByName("addBillForm")
             frm.submit(); //submit the form
-            frm.reset(); //reset all from data
             return false;
-
         }
     </script>
+
+<!--Script for opening and closing of the bill infor form-->
+<!--    <script>-->
+<!--        function openBillInfo(expenseName) {-->
+<!--            document.getElementById("info-from").style.display = "block";-->
+<!--            document.getElementById("form-container").style.display = "none";-->
+<!--            // var billName = document.getElementById("store_var").getAttribute("data-value");-->
+<!--            document.getElementById("expense_name").innerHTML = billName;-->
+<!--            // document.getElementById("date").innerHTML = dueDate;-->
+<!--        }-->
+<!--        function closeBillInfo() {-->
+<!--            document.getElementById("info-from").style.display = "none";-->
+<!--        }-->
+<!--    </script>-->
 </head>
 <body>
 
@@ -97,16 +110,15 @@ if (!$conn) {
     <button class="add_bill" onclick="openBillForm()"><span id="add_bill"> + Add Bill</span></button>
 </div>
 <div class="form-container" id="form-container">
-    <form action="process-form.php" method="post" name="addBillForm" onsubmit="textClear()">
+    <form action="/process-form.php" method="post" name="addBillForm" onsubmit="textClear()">
 
         <input type="text" id="Name" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="Enter Bill Name" name="Bill_Name" required>
         <input type="date" id="Date" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="EDate" name="Date" required>
-<!--Here the Placeholder for all the user will change as we have group formation page up and running. This is just for test of the functionality -->
+        <!--Here the Placeholder for all the user will change as we have group formation page up and running. This is just for test of the functionality -->
         <input type="number"  id="User_1" min="0.00" max="10000.00" step="0.01" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="User 1 Pays" name="U1" required>
         <input type="number"  id="User_2" min="0.00" max="10000.00" step="0.01" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="User 2 Pays" name="U2" required>
         <input type="number"  id="User_3" min="0.00" max="10000.00" step="0.01" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="User 3 Pays" name="U3" required>
         <input type="number"  id="User_4" min="0.00" max="10000.00" step="0.01" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" placeholder="User 4 Pays" name="U4" required>
-
 
         <button type="submit" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" class="btn">Submit</button>
         <button type="button" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 1.5vw" class="btn cancel" onclick="closeBillForm()">Cancel</button>
@@ -120,20 +132,40 @@ if (!$conn) {
 
 <div class="bill_section">
     <?php
-        $sql = "SELECT * FROM allExpenses;";
-        $result = mysqli_query($conn, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $expenseName = $row['expenseName'];
-            $dueDate = $row['dueDate'];
-            $amount = $row['totalAmt'];
-            echo '<div class="bill_box">
-    <span class="bill_Name">';$expenseName; '</span>
-    <span class="bill_Date">';$dueDate; '</span>
-    <span class="bill_Amount">'; $amount; '</span></div>';
-    echo '<style>.bill_box {
-        top: +20%; bottom: -20%;
-    }</style>';
+//    Here 'hGilmore909' is a test user in a group but it will be change to current user once group formation page is done.
+    $sql = "SELECT * FROM allExpenses WHERE (username = 'hGilmore909' OR user2 = 'hGilmore909' OR user3 = 'hGilmore909' OR user4 = 'hGilmore909')";
+    $result = mysqli_query($conn, $sql);
+    $top_increment = 3;
+    $i = 1;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $expenseName = $row['expenseName'];
+        $dueDate = $row['dueDate'];
+        $amount = $row['totalAmt'];
+        $top = $top_increment * $i;
+        $bill_box_id = "bill_box".$i;
+        echo '<div class="bill_box" id='.$bill_box_id.' onclick="window.open(\'Display_Expenses.php?expenseName='.$expenseName.'\', \'_blank\', \'width=600, height=400\')" style="top: '. $top .'%;">
+    <span class="bill_Name" id="bill_Name">'.$expenseName.'</span>
+    <span class="bill_Date" id="bill_Date">'.$dueDate.'</span>
+    <span class="bill_Amount" id="bill_Amount">'.$amount.'</span></div>';
+//        echo '<div id="store_var" data-value='.$row['expenseName'].' style="display: none"></div>';
+        $i++;
     }?>
 </div>
 
+
+<!--Not displaying fancy for Sprint 2 so working with the basic functionality for now-->
+<!--<div class="info-from" id="info-from">-->
+<!--    <form action="/info.php" name="BillInfoForm">-->
+<!---->
+<!--        <button type="button" style="font-family: 'Inter', sans-serif; font-style: normal; font-weight: 400; font-size: 2vw" class="back" onclick="closeBillInfo()">Back</button>-->
+<!---->
+<!--        <span id="expense_name"></span>-->
+<!--        <span id="date">Date: </span>-->
+<!---->
+<!---->
+<!--    </form>-->
+<!--</div>-->
+
+
 </body>
+</html>
