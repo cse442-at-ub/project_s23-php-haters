cb146c85c6339cf4c2ff69cc9f36b00667798b49
accartwr <89925308+accartwr@users.noreply.github.com>
2023-04-03 11:42:51 -0400

    fixed add-item button functionality #68

--- inventory.php
+++ inventory.php
@@ -38,14 +38,26 @@
         }
 
         /* main  Webpage  */
+        .title {
+            text-align: center;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        .title h2 {
+            font-size: 24px;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+
         body {
             background-color: #CDB7E9;
         }
 
         .scroll-box {
             background-color: #FFFFFF;
-            height: 70vh;
-            max-height: calc(70vh - 10vh);
+            height: 75vh;
+            max-height: calc(75vh - 10vh);
             overflow: auto;
             padding: 25px;
             position: fixed;
@@ -101,10 +113,94 @@
             border-radius: 5px;
             cursor: pointer;
             font-weight: bold;
-            font-size: 14px;
+            font-size: 50px;
             line-height: 1.5;
         }
 
+        #add-item-btn {
+            background-color: #EEE3A4;
+            color: #000000;
+            font-size: 24px;
+            padding: 10px 20px;
+            border: 3px solid #000000;
+            border-radius: 10px;
+            /*margin: 0 auto; !* center button horizontally for now...  *!*/
+            display: block; /* make button a block element to center it vertically for now */
+            /*margin-top: 10px; !* top margin to put space between button and nav bar!!  */
+        }
+
+        /*adding new item-  trying to fix layout for new items so it matches others */
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 25px; /* Add this line to match the margin of the other items */
+        }
+
+        form > * {
+            margin-bottom: 15px; /* this shud match the margin of the other items???...ahhh idk */
+        }
+
+        form label {
+            font-size: 20px;
+            margin-right: 10px;
+        }
+
+        form input {
+            padding: 5px;
+            font-size: 20px;
+            border: none;
+            border-bottom: 2px solid black;
+            background-color: #F3ECC1;
+        }
+
+        /* trying modal box to add an item */
+        #add-item-modal {
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        #add-item-modal form {
+            background-color: #F3ECC1;
+            margin: auto;
+            padding: 20px;
+            border: 2px solid #000000;
+            width: 80%;
+        }
+
+        #add-item-modal form label {
+            font-size: 20px;
+            margin-right: 10px;
+        }
+
+        #add-item-modal form input {
+            padding: 5px;
+            font-size: 20px;
+            border: none;
+            border-bottom: 2px solid black;
+            background-color: #FFFFFF;
+            margin-bottom: 10px;
+            width: 100%;
+        }
+        #add-item-modal form button[type="submit"],
+        #add-item-modal form button[type="button"] {
+            margin-top: 10px;
+            font-size: 20px;
+            font-weight: bold;
+            padding: 5px 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            background-color: #FFFFFF;
+            color: #000000;
+        }
     </style>
 </head>
 
@@ -127,26 +223,34 @@
     </div>
 </header>
 
-<!--  Buttons above scroll box. do we need tag stuff?   -->
+<!--  Buttons above the scroll box   -->
+
 <div>
-    <button class="btn" style="background-color: #EEE3A4; color: #000000;" onclick="showAddItemBox()">Add Item</button>
+    <button id="add-item-btn" class="btn" style="background-color: #EEE3A4; color: #000000;" onclick="showAddItemBox()">Add Item</button>
 </div>
-<div id="add-item-box" style="display: none;">
+
+
+<!--add item pop up box-->
+<div id="add-item-modal" style="display: none;">
     <form>
         <label for="item-name">Name:</label>
         <input type="text" id="item-name" name="item-name" required>
         <br>
         <label for="item-quantity">Quantity:</label>
-        <input type="number" id="item-quantity" name="item-quantity" required>
+        <input type="number" id="item-quantity" name="item-quantity" required min="0">
         <br>
         <label for="item-tag">Tag:</label>
         <input type="text" id="item-tag" name="item-tag" required>
         <br><br>
         <button class="btn" type="submit">Add</button>
-        <button class="btn" type="button" onclick="hideAddItemBox()">Cancel</button>
+        <button class="btn" type="button" onclick="hideAddItemBox()">Close</button>
     </form>
 </div>
 
+
+<div class="title">
+    <h2 style="font-family: sans-serif;">MY SHARED INVENTORY:</h2>
+</div>
 
 <!--SCROLL BAR STATIC OBJECTS FOR NOW?-->
 <div class="scroll-box">
@@ -178,9 +282,88 @@
 </div>
 
 <script>
-    // Event listeners for plus and minus buttons...querySelectorAll finds buttons
+    //const form = document.querySelector('form');
+    const modal = document.getElementById("add-item-modal");
+    const form = modal.querySelector("form");
+
+    // function showAddItemBox() {// javascript can dynamically show/hide forms to enter info ;)
+    //     document.getElementById("add-item-box").style.display = "block";
+    // }
+    function showAddItemBox() {
+        const modal = document.getElementById("add-item-modal");
+        modal.style.display = "block";
+    }
+
+    //
+    // function hideAddItemBox() {
+    //     document.getElementById("add-item-box").style.display = "none";
+    // }
+    function hideAddItemBox() {
+        const modal = document.getElementById("add-item-modal");
+        modal.style.display = "none";
+    }
+
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const name = document.getElementById('item-name').value;
+        const quantity = parseInt(document.getElementById('item-quantity').value);
+        const tag = document.getElementById('item-tag').value;
+        const item = {
+            name: name,
+            quantity: quantity,
+            tag: tag
+        };
+        addItem(item);
+        form.reset();
+        hideAddItemBox();
+    });
+    function addItem(item) {
+        const scrollBox = document.querySelector('.scroll-box');
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('item'); // Add 'item' class to the new item div
+        //const div = document.createElement('div'); //not looking good, try each el as own div
+
+        const div1 = document.createElement('div');
+        const nameSpan = document.createElement('span');
+        nameSpan.innerText = "Name: " + item.name;
+        const quantityBtnDiv = document.createElement('div');
+        quantityBtnDiv.classList.add('quantity-btn');
+        const minusBtn = document.createElement('button');
+        minusBtn.classList.add('btn', 'minus-btn');
+        minusBtn.setAttribute('data-id', item.name);
+        minusBtn.innerText = '-';
+        const quantitySpan = document.createElement('span');
+        quantitySpan.classList.add('quantity');
+        quantitySpan.innerText = item.quantity;
+        const plusBtn = document.createElement('button');
+        plusBtn.classList.add('btn', 'plus-btn');
+        plusBtn.setAttribute('data-id', item.name);
+        plusBtn.innerText = '+';
+        const div2 = document.createElement('div');
+        const tagSpan = document.createElement('span');
+        tagSpan.innerText = "Tag: " + item.tag;
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('btn');
+        removeBtn.innerText = 'Remove';
+        removeBtn.addEventListener('click', function() {
+            scrollBox.removeChild(itemDiv);
+        });
+        div1.appendChild(nameSpan);
+        quantityBtnDiv.appendChild(minusBtn);
+        quantityBtnDiv.appendChild(quantitySpan);
+        quantityBtnDiv.appendChild(plusBtn);
+        div1.appendChild(quantityBtnDiv);
+        div2.appendChild(tagSpan);
+        div2.appendChild(removeBtn);
+        itemDiv.appendChild(div1);
+        itemDiv.appendChild(div2);
+        scrollBox.appendChild(itemDiv);
+    }
+
     const minusBtns = document.querySelectorAll('.minus-btn');
     const plusBtns = document.querySelectorAll('.plus-btn');
+    // cant get to work with newly entered item^^  making new function to track new item buttons
 
     minusBtns.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -191,12 +374,14 @@
                 quantity--;
                 quantityEl.innerText = quantity;
                 if (quantity < 3) {
-                    btn.parentElement.parentElement.style.backgroundColor = "#E50303"; //do 40% of this??
+                    //btn.parentElement.parentElement.style.backgroundColor = "#E50303"; //do 40% of this??
+                    btn.parentElement.parentElement.style.backgroundColor = "#F27A93"; //lighter red looks bettr
                 }
             }
         });
     });
 
+    // plus button for quantity
     plusBtns.forEach(btn => {
         btn.addEventListener('click', () => {
             const itemId = btn.getAttribute('data-id');
@@ -204,16 +389,38 @@
             let quantity = parseInt(quantityEl.innerText);
             quantity++;
             quantityEl.innerText = quantity;
+            if (quantity >= 3) {
+                btn.parentElement.parentElement.style.backgroundColor = "#F3ECC1";
+            }
         });
     });
-      // javascript dynamically wil show box to enter info ;)
-    function showAddItemBox() {
-        document.getElementById("add-item-box").style.display = "block";
-    }
 
-    function hideAddItemBox() {
-        document.getElementById("add-item-box").style.display = "none";
-    }
-  
+    // adding another listener but only for newly added items
+    const newMinusBtn = itemDiv.querySelector('.minus-btn');
+    const newPlusBtn = itemDiv.querySelector('.plus-btn');
+
+    newMinusBtn.addEventListener('click', () => {
+        const quantityEl = itemDiv.querySelector('.quantity');
+        let quantity = parseInt(quantityEl.innerText);
+        if (quantity > 0) {
+            quantity--;
+            quantityEl.innerText = quantity;
+            if (quantity < 3) {
+                itemDiv.style.backgroundColor = "#F27A93";
+            }
+        }
+    });
+
+    newPlusBtn.addEventListener('click', () => {
+        const quantityEl = itemDiv.querySelector('.quantity');
+        let quantity = parseInt(quantityEl.innerText);
+        quantity++;
+        quantityEl.innerText = quantity;
+        if (quantity >= 3) {
+            itemDiv.style.backgroundColor = "#F3ECC1";
+        }
+    });
+
+
 </script>
 </body>
