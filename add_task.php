3e2df836b5a5ecdc1698dd3e74e64d42d5b2c1fc
BensonCai <bensonca@buffalo.edu>
2023-04-05 11:02:51 -0400

    added functions to get tasks and group name of user #64

--- add_task.php
+++ add_task.php
@@ -1,16 +1,22 @@
 <?php
+include 'util.php';
+
+session_start();
+//$name = $_SESSION["username"];
+$name = "Ben";
+
 
 $host = "oceanus.cse.buffalo.edu";              // The hostname of the database server
 $user = "bensonca";                             // The MySQL user
 $password = "50355548";                         // The MySQL user's password
 $database = "cse442_2023_spring_team_ae_db";    // The name of the database to connect to
 
+
 // Create a new mysqli object to establish a database connection
 $mysqli = mysqli_connect($host, $user, $password, $database);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
-
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_name = $_POST["task-name"];
@@ -19,19 +25,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 $email = "sampleuser1@gmail.com";
+$group_name = getGroupName($name, $mysqli);
 
 //$sql = "INSERT INTO tasks (task, importance, due_date) VALUES ('$task_name', '$importance', '$due_date')";
-// Prepare a SQL statement with placeholders for the user input
-$stmt = $mysqli->prepare("INSERT INTO tasks (task, importance, due_date, email) VALUES (?, ?, ?, ?)");
-
 // Bind the user input values to the prepared statement (strin, int, string, string)
-$stmt->bind_param("siss", $task_name, $importance, $due_date, $email);
-
 // Execute the prepared statement
+$stmt = $mysqli->prepare("INSERT INTO tasks (task, importance, due_date, email, groupName) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sisss", $task_name, $importance, $due_date, $email, $group_name);
 $stmt->execute();
-
-//$sql = "INSERT INTO tasks VALUES ('$task_name', '$importance', '$due_date', '$email')";
-//mysqli_query($mysqli, $sql);
 
 $stmt->close();
 $mysqli->close();
---
+++ util.php
@@ -0,0 +1,50 @@
+<?php
+
+$host = "oceanus.cse.buffalo.edu";              // The hostname of the database server
+$user = "bensonca";                             // The MySQL user
+$password = "50355548";                         // The MySQL user's password
+$database = "cse442_2023_spring_team_ae_db";    // The name of the database to connect to
+
+$mysqli = mysqli_connect($host, $user, $password, $database);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+function connect(){
+    $host = "oceanus.cse.buffalo.edu";              // The hostname of the database server
+    $user = "bensonca";                             // The MySQL user
+    $password = "50355548";                         // The MySQL user's password
+    $database = "cse442_2023_spring_team_ae_db";    // The name of the database to connect to
+
+    return mysqli_connect($host, $user, $password, $database);
+}
+
+function getGroupName($username, $mysqli){
+    $name = $username;
+    $stmt = $mysqli->prepare("SELECT groupName FROM `groups` WHERE usersUsername = ? OR user2 = ? OR user3 = ? OR user4 = ?");
+    $stmt->bind_param("ssss", $name, $name, $name, $name);
+    $stmt->execute();               // Find the group the user is in
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $stmt->close();
+    $mysqli->close();
+    return $row["groupName"];
+}
+
+function getTasks($groupName, $mysqli){
+//    echo $groupName;
+
+    $stmt = $mysqli->prepare("SELECT * FROM tasks WHERE groupName = ?");
+    $stmt->bind_param("s", $groupName);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    $mysqli->close();
+
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
