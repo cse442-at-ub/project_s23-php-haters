b4a16e8cda7b682fa685a7dcc9adbc62929b5f31
r14dd <73396868+r14dd@users.noreply.github.com>
2023-04-10 12:32:28 -0400

    Updated the task sending #65

--- notification.php
+++ notification.php
@@ -18,7 +18,26 @@ $twenty_four_hours = 60 * 60 * 24;
 $reminder_due_date_1 = date('Y-m-d H:i:s', $now + $three_days);
 $reminder_due_date_2 = date('Y-m-d H:i:s', $now + $twenty_four_hours);
 $reminder_due_date_3 = date('Y-m-d H:i:s', $now + $twenty_four_hours + $twenty_four_hours);
-$sql = "SELECT task, due_date, email FROM tasks WHERE (due_date BETWEEN '$reminder_due_date_1' AND '$reminder_due_date_2') OR (due_date BETWEEN '$reminder_due_date_2' AND '$reminder_due_date_3')";
+
+// Tasks due in less than 3 days
+$sql = "SELECT task, due_date, email FROM tasks WHERE (due_date BETWEEN '$reminder_due_date_1' AND '$reminder_due_date_2')";
+$result = mysqli_query($conn, $sql);
+
+// Loop through the tasks and send reminder emails
+while ($row = mysqli_fetch_assoc($result)) {
+    $task_title = $row['task'];$task_due_date = $row['due_date'];
+    $user_email = $row['email'];
+
+    // send reminder email
+    $to = $user_email;
+    $subject = 'RoomAid Task Reminder';
+    $message = "This is a reminder that you have a task, $task_title, due on $task_due_date within the next 3 days.";
+    // sending the email
+    mail($to, $subject, $message);
+}
+
+// Tasks due in less than 24 hours
+$sql = "SELECT task, due_date, email FROM tasks WHERE (due_date BETWEEN '$reminder_due_date_2' AND '$reminder_due_date_3')";
 $result = mysqli_query($conn, $sql);
 
 // Loop through the tasks and send reminder emails
@@ -26,15 +45,13 @@ while ($row = mysqli_fetch_assoc($result)) {
     $task_title = $row['task'];
     $task_due_date = $row['due_date'];
     $user_email = $row['email'];
-
+    
     // send reminder email
     $to = $user_email;
     $subject = 'RoomAid Task Reminder';
-    $message = "This is a reminder that you have a task, $task_title, due on $task_due_date.";
+    $message = "This is a reminder that you have a task, $task_title, due on $task_due_date within the next 24 hours.";
     // sending the email
     mail($to, $subject, $message);
 }
-
 // Close the database connection
 mysqli_close($conn);
-
