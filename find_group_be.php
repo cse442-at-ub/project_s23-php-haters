6911027b85272232fc31c0874c526f103ef6915d
Arpit Hirani <70348757+arpithirani@users.noreply.github.com>
2023-04-11 21:35:51 -0400

    Fully functional group seach page #44
    Moving local files to my branch.

---
+++ find_group_be.php
@@ -0,0 +1,80 @@
+<?php
+
+session_start();
+
+//Database Connection
+$host = "oceanus.cse.buffalo.edu";
+$user = "arpithir";
+$pass = "50340819";
+$database = "cse442_2023_spring_team_ae_db";
+
+//make sure we found oceanus
+$conn = mysqli_connect($host, $user, $pass, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['group'])) { 
+    if(isset($_SESSION['username'])){ // check if user session variable is set
+        $current_user = $_SESSION['username'];
+        $grpName = $_POST['group'];
+        $sql = "SELECT * FROM groupTestV2 WHERE groupName = '$grpName'";
+        $result = mysqli_query($conn,$sql);
+        $result_val = mysqli_fetch_assoc($result);
+        $group_name = $result_val['groupName'];
+        if (is_null($group_name)){
+            $arr = "INSERT INTO groupTestV2 VALUES ('$current_user', '$grpName')";
+            //  Block pesky SQL injections with prepared statement ;)
+            $stmt = $conn->prepare($arr);
+            $stmt->bind_param("ss", $current_user, $grpName); // BIND
+            $stmt->execute();
+            echo "<script>window.location.href='group.php';</script>";
+            $stmt->close();
+        }
+        else {
+            $_SESSION['error'] = 'Sorry, the group name already exists. Pick a new group name.';
+            echo '<script>alert("' . $_SESSION['error'] . '"); window.location.href = "find_group.php";</script>';
+            exit();
+        }
+
+    }
+}
+
+if (isset($_POST['search'])) {
+    addUser($conn);
+}
+
+function addUser($conn) {
+    if(isset($_SESSION['username'])){ // check if user session variable is set
+        $current_user = $_SESSION['username'];
+        $search_val = $_POST['search'];
+        $sql = "SELECT * FROM groupTestV2 WHERE groupName = '$search_val'";
+        $result = mysqli_query($conn,$sql);
+        $result_val = mysqli_fetch_assoc($result);
+        $group_name = $result_val['groupName'];
+        if (!is_null($group_name)){
+            $sql2 = "SELECT COUNT(*) AS total FROM groupTestV2 WHERE groupName = '$search_val'";
+            $num = mysqli_query($conn,$sql2);
+            $num_arr = mysqli_fetch_assoc($num);
+            $num_people = $num_arr['total'];
+            if ($num_people >= 4){
+                $_SESSION['error'] = 'Sorry, the group is currently full. Create a new group or Join a different group.';
+                echo '<script>alert("' . $_SESSION['error'] . '"); window.location.href = "find_group.php";</script>';
+                exit();
+            }
+            if ($num_people < 4) {
+                $arr = "INSERT INTO groupTestV2 VALUES ('$current_user', '$search_val')";
+                $stmt = $conn->prepare($arr);
+                $stmt->execute();
+                echo "<script>window.location.href='group.php';</script>";
+                $stmt->close();
+            }  
+        }
+        else {
+            $_SESSION['error'] = 'Sorry, there is no group with that name.';
+            echo '<script>alert("' . $_SESSION['error'] . '"); window.location.href = "find_group.php";</script>';
+        }
+    }
+}
+
+
