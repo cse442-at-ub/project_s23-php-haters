bcd9c1a96ef145ee5f00036579c089f155110e0c
BensonCai <bensonca@buffalo.edu>
2023-05-05 02:28:26 -0400

    image loading to navbar bug fix #124

--- Shared_Expenses.php
+++ Shared_Expenses.php
@@ -1,6 +1,6 @@
 <?php
+include 'header.php';
 
-session_start();
 $current_user = $_SESSION['username'];
 
 
@@ -88,22 +88,22 @@ $graph = mysqli_query($conn, $sql);
 </head>
 <body>
 
-<header>
-    <div>
-        <img class='icon' src="Saturn.png" alt="RoomAid">
-        <span class="h3">RoomAid</span>
-        <nav>
-            <ul>
-                <li><a href="home.php" class="nav-button">Home</a></li>
-                <li><a href="task-schedule.php" class="nav-button">Schedule</a></li>
-                <li><a href="group.php" class="nav-button">Group</a></li>
-                <li><a href="inventory.php" class="nav-button">Inventory</a></li>
-                <li><a href="Shared_Expenses.php" class="nav-button">Expenses</a></li>
-            </ul>
-        </nav>
-        <a href="ppage.php"><img id='icon-pfp' src="profile.png" alt="Profile"></a>
-    </div>
-</header>
+<!--<header>-->
+<!--    <div>-->
+<!--        <img class='icon' src="Saturn.png" alt="RoomAid">-->
+<!--        <span class="h3">RoomAid</span>-->
+<!--        <nav>-->
+<!--            <ul>-->
+<!--                <li><a href="home.php" class="nav-button">Home</a></li>-->
+<!--                <li><a href="task-schedule.php" class="nav-button">Schedule</a></li>-->
+<!--                <li><a href="group.php" class="nav-button">Group</a></li>-->
+<!--                <li><a href="inventory.php" class="nav-button">Inventory</a></li>-->
+<!--                <li><a href="Shared_Expenses.php" class="nav-button">Expenses</a></li>-->
+<!--            </ul>-->
+<!--        </nav>-->
+<!--        <a href="ppage.php"><img id='icon-pfp' src="profile.png" alt="Profile"></a>-->
+<!--    </div>-->
+<!--</header>-->
 
 
 <div class="add_bill">
